4b319558e34bbe26492ea5bb986269760e412752
Juampi REP <girardijuanpablo11@gmail.com>
2024-10-21 13:19:24 -0300

    Hago el array principal de los productos de mi página

--- index.js
+++ index.js
@@ -1,31 +1,101 @@
-function calculoIva(precioProducto){
-    let interesIva=0.21;
-    function iva(){
-        return precioProducto*interesIva;
-    }
-    let sumarIva=precioProducto+iva();
-    return sumarIva;
-}
-console.log(calculoIva(2000));
+const productos = [
 
+    // LATAS
 
-const PROD_1=1000;
-const PROD_2=2000;
-const PROD_3=1500;
-const PROD_4=1250;
-const PROD_5=4000;
-const PROD_6=2000;
-const PROD_7=7000;
-const PROD_8=2500;
-const PROD_9=5200;
-const PROD_10=500;
-const PROD_11=1100;
-const PROD_12=3500;
+    {
+        tipo: "latas",
+        nombre: "Summer Ale",
+        precio: 2000,
+        medida: "500ml"
+    },
+    
+    {
+        tipo: "latas",
+        nombre: "Florida Honey",
+        precio: 2000,
+        medida: "500ml"
+    },
 
-let confirmacion=parseInt(prompt("Desea comprar algun producto:(1=Si o 2=No)"));
-let producto=0;
-while(confirmacion===1){
-    producto=producto+parseInt(prompt("Elige el producto que deseas comprar por su precio:(1000, 2000, 1500, 1250, 4000, 2000, 7000, 2500, 5200, 500, 1100, 3500)"));
-    confirmacion=parseInt(prompt("Desea comprar algun otro producto:(1=Si o 2=No)"));
-}
-console.log("Total a pagar: "+producto);
+    {
+        tipo: "latas",
+        nombre: "English Porter",
+        precio: 2000,
+        medida: "500ml"
+    },
+
+    {
+        tipo: "latas",
+        nombre: "Session Ipa",
+        precio: 2000,
+        medida: "500ml"
+    },
+
+    {
+        tipo: "latas",
+        nombre: "American Ipa",
+        precio: 2000,
+        medida: "500ml"
+    },
+
+    // CHOPERAS
+
+    // 20LT
+
+    {
+        tipo: "chopera",
+        nombre: "Summer Ale",
+        precio: 10000,
+        medida: "20lt"
+    },
+
+    {
+        tipo: "chopera",
+        nombre: "Rubia",
+        precio: 10000,
+        medida: "20lt"
+    },
+
+    {
+        tipo: "chopera",
+        nombre: "American Ipa",
+        precio: 10000,
+        medida: "20lt"
+    },
+
+    {
+        tipo: "chopera",
+        nombre: "Session Ipa",
+        precio: 10000,
+        medida: "20lt"
+    },
+
+    // 50LT
+
+    {
+        tipo: "chopera",
+        nombre: "Summer Ale",
+        precio: 22500,
+        medida: "50lt"
+    },
+
+    {
+        tipo: "chopera",
+        nombre: "Rubia",
+        precio: 22500,
+        medida: "50lt"
+    },
+
+    {
+        tipo: "chopera",
+        nombre: "American Ipa",
+        precio: 22500,
+        medida: "50lt"
+    },
+
+    {
+        tipo: "chopera",
+        nombre: "Session Ipa",
+        precio: 22500,
+        medida: "50lt"
+    },
+]
